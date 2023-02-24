7786a4eb65af3521da727b697878810410a66cad
chang <m18555309923@163.com>
2023-02-24 17:36:46 +0800

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+// import { createApp } from 'vue'
+// import './style.css'
+// import App from './App.vue'
+//
+// createApp(App).mount('#app')
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import 'ant-design-vue/dist/antd.less'
+import 'uno.css'
+import '@/styles/Design/index.less'
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
---
+++ vite.config.ts
@@ -0,0 +1,46 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { fileURLToPath, URL } from 'node:url'
+// 实现组件库或内部组件的自动按需引入组件
+import components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver as antDesignVueResolver } from 'unplugin-vue-components/resolvers'
+// 解决 unplugin-vue-components 插件 加载UI库中无法处理的非组件模块
+import usePluginImport from 'vite-plugin-importer'
+// 使用Unocss
+import unoCss from 'unocss/vite'
+import presetUno from 'unocss/preset-uno'
+import {resolve} from "path";
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        vue(),
+        vueJsx(),
+        components({
+            resolvers: [antDesignVueResolver({ importStyle: false })]
+        }),
+        usePluginImport({
+            libraryName: 'ant-design-vue',
+            libraryDirectory: 'es',
+            style: (name: any) => `${name}/style/css`
+        }),
+        unoCss({
+            presets: [presetUno()]
+        })
+    ],
+    css: {
+        preprocessorOptions: {
+            less: {
+                modifyVars: {
+                    hack: `true; @import (reference) "${resolve('src/styles/Design/config.less')}";`
+                },
+                javascriptEnabled: true
+            }
+        }
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    }
+})
